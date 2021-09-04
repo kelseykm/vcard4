db04e1a189908082cba043b9a465951456590ec5
Kelsey Mwongeli <kelseykm.git@gmail.com>
2021-09-04 22:22:48 +0300

    Added index.js as gateway to lib

---
+++ index.js
@@ -0,0 +1,4 @@
+export * as values from './lib/values.js';
+export * as parameters from './lib/parameters.js';
+export * as properties from './lib/properties.js';
+export { default as VCARD } from './lib/card.js';
