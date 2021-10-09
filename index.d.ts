6eee936dd77526670b7c6bafc52ff66b31303d89
Kelsey Mwongeli <kelseykm.git@gmail.com>
2021-10-09 17:55:52 +0300

    Revert "Merge pull request #2 from MarcelWaldvogel/types"
    This reverts commit 798848e22989bf5b002d33e6d0a1f03a38f0a9c0
    
    Faulty code had been introduced with the merged pull request

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export * as errors from "./src/esm/errors.js";
-export * as values from "./src/esm/values.js";
-export * as parameters from "./src/esm/parameters.js";
-export * as properties from "./src/esm/properties.js";
-export { default as VCARD } from "./src/esm/card.js";
-export { default as parse } from "./src/esm/parse.js";
--- src/esm/errors.d.ts
+++
@@ -1,6 +0,0 @@
-export class MissingArgument extends Error {
-    constructor(message: any);
-}
-export class InvalidArgument extends Error {
-    constructor(message: any);
-}
