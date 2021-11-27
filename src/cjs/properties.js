b61bc4ee9aa596fdc0803f4c751ce04bc746740d
Kelsey Mwongeli <kelseykm.git@gmail.com>
2021-11-27 17:10:53 +0300

    Added property from RFC8605 extension
    - ContactURIProperty

--- src/cjs/properties.js
+++ src/cjs/properties.js
@@ -1330,6 +1330,61 @@ class LangProperty extends AbstractBaseProperty {
   }
 }
 
+class ContactURIProperty extends AbstractBaseProperty {
+  static prop = 'CONTACT-URI';
+  static cardinality = '*';
+  static acceptableParamTypes = [
+    ValueParameter,
+    PrefParameter,
+  ];
+  static acceptableValTypes = URIType;
+
+  #validate(params, value) {
+    if (typeof params === 'undefined' || typeof value === 'undefined')
+    throw new MissingArgument('Parameters and value for ContactURIProperty must be supplied');
+
+    else if (!Array.isArray(params))
+    throw new InvalidArgument('Parameters for ContactURIProperty must be passed in an array');
+
+    else if (
+      !params.every(
+        param => this.constructor.acceptableParamTypes.some(
+          acceptableParamType => {
+            if (acceptableParamType === ValueParameter)
+            return (
+              (param instanceof acceptableParamType) &&
+              (param.value === 'uri')
+            );
+
+            return param instanceof acceptableParamType;
+          }
+        )
+      )
+    )
+    throw new TypeError('Some of the parameters passed are not valid parameters for ContactURIProperty');
+
+    else if (!(value instanceof this.constructor.acceptableValTypes))
+    throw new TypeError('Invalid type for value of ContactURIProperty');
+
+    else if (!/^(mailto|https?)/.test(value.repr()))
+    throw new InvalidArgument('Value for ContactURIProperty must be a "mailto", "http", or "https" URI value');
+  }
+
+  constructor(params, val) {
+    super();
+
+    this.#validate(params, val);
+    this.params = params.reduce((parametersArray, currentParameter) => {
+      parametersArray.push(currentParameter.repr());
+      return parametersArray;
+    }, []).join(';');
+    this.value = val.repr();
+
+    this.checkAbstractPropertiesAndMethods();
+    Object.freeze(this);
+  }
+}
+
 // Geographical Properties
 class TzProperty extends AbstractBaseProperty {
   static prop = 'TZ';
@@ -2721,6 +2776,7 @@ module.exports = {
   EmailProperty,
   IMPPProperty,
   LangProperty,
+  ContactURIProperty,
   TzProperty,
   GeoProperty,
   TitleProperty,
