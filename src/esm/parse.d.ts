75dbd2318f666d02874797ac9102f7c081df684a
Kelsey Mwongeli <kelseykm.git@gmail.com>
2021-12-09 21:40:59 +0300

    Bug fix
    Types for parse

--- src/esm/parse.d.ts
+++ src/esm/parse.d.ts
@@ -1,18 +1,20 @@
-declare type valueType = {
-  [key: string]: {
-    [key: string]: string | string[]
-  };
-} | string;
-
-declare type parameterType = {
-  [key: string]: {
-    [key: string]: string | string[]
-  }
-} | string | undefined;
-
-declare type parsedVcard = {
-  parameter: parameterType;
-  value: valueType;
+declare type ParsedVcardValue =  string | string[] | {
+  [key: string]: string | string[];
 };
 
-export default function parse(vcard: string): parsedVcard | parsedVcard[];
+declare type ParsedVcardParameter = undefined | { 
+  [key: string]: string | string[]; 
+} | { 
+  [key: string]: string | string[]; 
+}[];
+
+declare type ParsedVcardContentLine = {
+  parameters: ParsedVcardParameter;
+  value: ParsedVcardValue;
+};
+
+declare type ParsedVcard = {
+[key: string]: ParsedVcardContentLine | ParsedVcardContentLine[];
+};
+
+export default function parse(vcard: string): ParsedVcard | ParsedVcard[];
