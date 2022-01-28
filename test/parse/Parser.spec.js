3b3a2e12287ee6419e57b7ce4512002b5fa1112c
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-01-28 20:21:06 +0300

    Test for case insensitivity in parse

--- test/parse/Parser.spec.js
+++ test/parse/Parser.spec.js
@@ -6,19 +6,19 @@ describe('Parser tests', () => {
   it('Accepts valid input', () => {
     assert.doesNotThrow(() => new Parser(new Tokenizer(
       'BEGIN:VCARD\r\n' +
-      'VERSION:4.0\r\n' +
+      'VErSiON:4.0\r\n' +
       'N:Doe;John;;;\r\n' +
       'FN:John Doe\r\n' +
       'ORG:Example.com Inc.;Marketing\r\n' +
-      'TITLE:Imaginary test person\r\n' +
-      'EMAIL;TYPE=work;PREF=1:johnDoe@example.org\r\n' +
+      'TiTlE:Imaginary test person\r\n' +
+      'EmAIL;TYPE=work;PREF=1:johnDoe@example.org\r\n' +
       'TEL;TYPE=cell:tel:+1 781 555 1212\r\n' +
-      'TEL;TYPE=home:tel:+1 202 555 1212\r\n' +
+      'tel;TYPE=home:tel:+1 202 555 1212\r\n' +
       'NOTE:John Doe has a long and varied history\\, being documented on more poli\r\n' +
       ' ce files than anyone else. Reports of his death are alas numerous.\r\n' +
       'CATEGORIES:Work,Test group\r\n' +
       'X-ABUID:5AD380FD-B2DE-4261-BA99-DE1D1DB52FBE\\:ABPerson\r\n' +
-      'END:VCARD\r\n'
+      'EnD:VCARD\r\n'
     ).tokens));
 
   });
