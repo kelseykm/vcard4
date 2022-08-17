33011b547fa160d15d139401834a8b8bbbb1ce60
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-08-17 21:18:48 +0300

    [fix](types) Add `Group` to accepted prop type for `VCARD`
    Closes #16

--- types/src/card.d.ts
+++ types/src/card.d.ts
@@ -45,9 +45,11 @@ import {
   ExtendedProperty
 } from './properties/index';
 
+import { Group } from './group'
+
 export class VCARD {
   repr(): string;
   reprXML(): string;
   reprJSON(): {}[];
-  constructor(props: (SourceProperty | KindProperty | XMLProperty | FNProperty | NProperty | NicknameProperty | PhotoProperty | BdayProperty | AnniversaryProperty | GenderProperty | BirthPlaceProperty | DeathPlaceProperty | DeathDateProperty | ExpertiseProperty | HobbyProperty | InterestProperty | AdrProperty | TelProperty | EmailProperty | IMPPProperty | LangProperty | ContactURIProperty | TzProperty | GeoProperty | TitleProperty | RoleProperty | LogoProperty | OrgProperty | MemberProperty | RelatedProperty | OrgDirectoryProperty | CategoriesProperty | NoteProperty | ProdidProperty | RevProperty | SoundProperty | UIDProperty | ClientpidmapProperty | URLProperty | KeyProperty | FburlProperty | CaladruriProperty | CaluriProperty | ExtendedProperty)[]);
+  constructor(props: (Group | SourceProperty | KindProperty | XMLProperty | FNProperty | NProperty | NicknameProperty | PhotoProperty | BdayProperty | AnniversaryProperty | GenderProperty | BirthPlaceProperty | DeathPlaceProperty | DeathDateProperty | ExpertiseProperty | HobbyProperty | InterestProperty | AdrProperty | TelProperty | EmailProperty | IMPPProperty | LangProperty | ContactURIProperty | TzProperty | GeoProperty | TitleProperty | RoleProperty | LogoProperty | OrgProperty | MemberProperty | RelatedProperty | OrgDirectoryProperty | CategoriesProperty | NoteProperty | ProdidProperty | RevProperty | SoundProperty | UIDProperty | ClientpidmapProperty | URLProperty | KeyProperty | FburlProperty | CaladruriProperty | CaluriProperty | ExtendedProperty)[]);
 }
