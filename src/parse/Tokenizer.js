kelseykm/vcard4

commit 6dbf1309238b0f45dffff113044f1112c2a82d85

fix(parser): parameter value unescaping

src/parse/Tokenizer.js
@@ -313,7 +313,12 @@ export class Tokenizer {
           holdCurrentParamValue.slice(1, -1)
         );
 
-      currentParam[currentParamKey] = holdCurrentParamValue;
+      currentParam[currentParamKey] = holdCurrentParamValue
+        .replaceAll("\\n", "\n")
+        .replaceAll("\\;", ";")
+        .replaceAll("\\:", ":")
+        .replaceAll("\\,", ",")
+        .replaceAll("\\\\", "\\");
     }
 
     return paramList.reduce((accumulatedParams, currentParam) => {

commit d44e26b26c7d3dc59579a69883c920f4d4177bfb

fix(parser): map replaceAll if value is array
bug introduced in commit 6dbf1309238b0f45dffff113044f1112c2a82d85

src/parse/Tokenizer.js
@@ -313,12 +313,24 @@ export class Tokenizer {
           holdCurrentParamValue.slice(1, -1)
         );
 
-      currentParam[currentParamKey] = holdCurrentParamValue
-        .replaceAll("\\n", "\n")
-        .replaceAll("\\;", ";")
-        .replaceAll("\\:", ":")
-        .replaceAll("\\,", ",")
-        .replaceAll("\\\\", "\\");
+      if (Array.isArray(holdCurrentParamValue))
+        holdCurrentParamValue.map((val) =>
+          val
+            .replaceAll("\\n", "\n")
+            .replaceAll("\\;", ";")
+            .replaceAll("\\:", ":")
+            .replaceAll("\\,", ",")
+            .replaceAll("\\\\", "\\")
+        );
+      else
+        holdCurrentParamValue
+          .replaceAll("\\n", "\n")
+          .replaceAll("\\;", ";")
+          .replaceAll("\\:", ":")
+          .replaceAll("\\,", ",")
+          .replaceAll("\\\\", "\\");
+
+      currentParam[currentParamKey] = holdCurrentParamValue;
     }
 
     return paramList.reduce((accumulatedParams, currentParam) => {