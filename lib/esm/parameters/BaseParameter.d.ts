9a08c59354dc76d28e33e3820fe68c3fbc08976b
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-12-12 23:51:25 +0300

    chore: track lib/**/BaseParameter.d.ts

---
+++ lib/esm/parameters/BaseParameter.d.ts
@@ -0,0 +1,16 @@
+declare class BaseParameter {
+  /**
+   * @returns Representation of how the parameter will appear in the vCard
+   */
+  repr(): string;
+  /**
+   * @returns Representation of how the parameter will appear in the XML vCard
+   */
+  reprXML(): string;
+  /**
+   * @returns Representation of how the parameter will appear in the jCard
+   */
+  reprJSON(): Record<string, string | string[]>;
+}
+
+export { BaseParameter };
