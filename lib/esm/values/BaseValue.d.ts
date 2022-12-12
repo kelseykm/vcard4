0e92ce6d6d43de59fa31c40b94398666f31911c9
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-12-12 21:36:55 +0300

    chore: track lib/**/BaseValue.d.ts

---
+++ lib/esm/values/BaseValue.d.ts
@@ -0,0 +1,17 @@
+declare class BaseValue {
+  /**
+   * @returns Representation of how the value type will appear in the vCard.
+   */
+  repr(): string;
+  /**
+   * @returns Representation of how the value type will appear in the XML
+   * vCard.
+   */
+  reprXML(): string;
+  /**
+   * @returns Representation of how the value type will appear in the jCard.
+   */
+  reprJSON(): {}[];
+}
+
+export { BaseValue };
