29192298fe3dac82543f4acfd3c138c53f7eda2a
Kelsey Mwongeli <kelseykm.git@gmail.com>
2025-02-13 14:41:14 +0300

    test: additional tests for `DateTimeType`
    Didn't have tests to catch errors in tests for Februarys

--- test/values/DateTimeType.spec.js
+++ test/values/DateTimeType.spec.js
@@ -5,17 +5,20 @@ describe("DateTimeType tests", () => {
   it("Accepts valid input", () => {
     assert.doesNotThrow(() => new DateTimeType("date", "1985-04"));
     assert.doesNotThrow(() => new DateTimeType("date", "---12"));
+    assert.doesNotThrow(() => new DateTimeType("date", "20250219"));
 
     assert.doesNotThrow(() => new DateTimeType("time", 1400));
     assert.doesNotThrow(() => new DateTimeType("time", "102200Z"));
 
     assert.doesNotThrow(() => new DateTimeType("datetime", "19961022T140000"));
+    assert.doesNotThrow(() => new DateTimeType("datetime", "19970219T140000"));
     assert.doesNotThrow(() => new DateTimeType("datetime", "---22T14"));
 
     assert.doesNotThrow(() => new DateTimeType("dateandortime", "T10"));
     assert.doesNotThrow(() => new DateTimeType("dateandortime", "---12"));
 
     assert.doesNotThrow(() => new DateTimeType("timestamp", "19961022T140000"));
+    assert.doesNotThrow(() => new DateTimeType("timestamp", "19970209T140000"));
     assert.doesNotThrow(
       () => new DateTimeType("timestamp", "19961022T140000-05")
     );
@@ -26,6 +29,8 @@ describe("DateTimeType tests", () => {
 
   it("Rejects invalid input", () => {
     assert.throws(() => new DateTimeType());
+    assert.throws(() => new DateTimeType("date", "20250431"));
+    assert.throws(() => new DateTimeType("date", "20250229"));
     assert.throws(() => new DateTimeType("timestamp", "19961022T14"));
     assert.throws(() => new DateTimeType("time", "T102200Z"));
     assert.throws(() => new DateTimeType({}));
