0dc366afb6b83ec64dc229b227a60fe2979539a8
Kelsey Mwongeli <kelseykm.git@gmail.com>
2025-12-11 23:16:09 +0300

    chore: build lib/ and dist/

--- lib/esm/parameters/LabelParameter.d.ts
+++ lib/esm/parameters/LabelParameter.d.ts
@@ -1,4 +1,6 @@
 import { BaseParameter } from './BaseParameter.js';
+import { ParameterValueType } from '../values/ParameterValueType.js';
+import '../values/BaseValue.js';
 
 /**
  * Represents the "LABEL" parameter
@@ -7,11 +9,10 @@ import { BaseParameter } from './BaseParameter.js';
  */
 declare class LabelParameter extends BaseParameter {
   /**
-   * @param value - plain-text string representing the formatted address.
-   * @example
-   * "Mr. John Q. Public, Esq.\nMail Drop: TNE QB\n123 Main Street\nAny Town, CA  91921-1234\nU.S.A."
+   * @param value - an instance of `ParameterValueType` with the formatted
+   * address.
    */
-  constructor(value: string);
+  constructor(value: ParameterValueType);
 }
 
 export { LabelParameter };
